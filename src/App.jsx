1fc64eb460a91513df5c19513f90ca637f823ba8
Jakub Kowieski <01158762@pw.edu.pl>
2021-10-06 00:58:44 +0200

    Inital commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Homepage from "./homepage.compontent";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <Homepage />
+    </div>
+  );
+}
+
+export default App;
