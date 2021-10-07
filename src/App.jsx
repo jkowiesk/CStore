1a90d705baacec5cff26f7c3eb29aaca46e1cd5a
Jakub Kowieski <01158762@pw.edu.pl>
2021-10-07 20:50:51 +0200

    Added Routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,24 @@
 import React from "react";
 import Homepage from "./pages/homepage/homepage.compontent";
+import { Switch, Route } from "react-router-dom";
 
 import "./App.css";
+
+function HatsPage() {
+  return (
+    <div>
+      <h1>HatsPage</h1>
+    </div>
+  );
+}
 
 function App() {
   return (
     <div>
-      <Homepage />
+      <Switch>
+        <Route exact path="/" component={Homepage} />
+        <Route path="/shop/hats" component={HatsPage} />
+      </Switch>
     </div>
   );
 }
