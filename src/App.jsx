b8592887ea82402295b1acc658405b7dc1579fe0
Jakub Kowieski <01158762@pw.edu.pl>
2021-10-27 00:05:29 +0200

    Updated page with preview component and item component

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,16 @@
 import React from "react";
-import Homepage from "./pages/homepage/homepage.compontent";
+import Homepage from "./pages/homepage/homepage.component";
 import { Switch, Route } from "react-router-dom";
+import ShopPage from "./pages/shop/shop.component";
 
 import "./App.css";
-
-function HatsPage() {
-  return (
-    <div>
-      <h1>HatsPage</h1>
-    </div>
-  );
-}
 
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path="/" component={Homepage} />
-        <Route path="/shop/hats" component={HatsPage} />
+        <Route path="/shop" component={ShopPage} />
       </Switch>
     </div>
   );
